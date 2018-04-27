3661fd8626bee4f43a04efbdb97ce7dab30acf04
明先均 <mingxianjun@163.com>
2018-04-28 00:03:30 +0800

    solving style conflicts

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,17 +3,24 @@
         <router-link to="/home" class="logo"></router-link>
         <Form :model="user">
             <FormItem prop="user">
-                <Input type="text" v-model="user.name" placeholder="邮箱">
-                <Icon type="ios-person-outline" slot="prepend"></Icon>
+                <Input type="text" size="large" v-model="user.name" placeholder="邮箱">
+                <Icon size="18" type="ios-person-outline" slot="prepend"></Icon>
                 </Input>
             </FormItem>
             <FormItem prop="password">
-                <Input type="password" v-model="user.password" placeholder="密码">
-                <Icon type="ios-locked-outline" slot="prepend"></Icon>
+                <Input size="large" type="password" v-model="user.password" placeholder="密码">
+                <Icon size="18" type="ios-locked-outline" slot="prepend"></Icon>
                 </Input>
             </FormItem>
             <FormItem>
-                <Button type="primary" @click="handleSubmit('user')">登录</Button>
+                <Button long size="large" type="primary" @click="submit('user')">登录</Button>
+            </FormItem>
+            <FormItem>
+                <router-link to="/forget" style="margin-right: 10px">忘记密码</router-link>
+                <router-link to="/register">免费注册</router-link>
+                <CheckboxGroup v-model="user.remember" style="float:right">
+                    <Checkbox label="记住我"></Checkbox>
+                </CheckboxGroup>
             </FormItem>
         </Form>
     </div>
@@ -27,6 +34,11 @@
         data(){
             return {
                 user:{}
+            }
+        },
+        methods:{
+            submit:function (user) {
+               console.log(user)
             }
         }
     }
