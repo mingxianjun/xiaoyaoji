dbbae4012f53048513c7f7e01a28f167eb6b591f
lemon <mingxianjun@163.com>
2018-05-08 15:48:48 +0800

    add 404page

--- src/views/common/notFound.vue
+++ src/views/common/notFound.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>404 Not Found</h1>
+    <div class="warp">
+        <span class="title">404</span><br/>
+        <span>请求的资源未找到！</span>
+        <a href="#/home">返回首页</a>
+    </div>
 </template>
 
 <script>
@@ -9,5 +13,14 @@
 </script>
 
 <style scoped>
-    h1{color: red;}
+    .warp{
+        height: 100%;
+        padding-top: 20%;
+        text-align: center;
+        color: #fff;
+        background: rgba(0, 188, 212, .5);
+    }
+    .warp .title{
+        font-size: 100px;
+    }
 </style>
